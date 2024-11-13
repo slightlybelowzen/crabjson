2a97cf6fb946f06b9ffd2cd412b89f6c02070d01
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2024-11-13 15:37:46 +0000

    started with cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("usage: crabjson [file ...]");
+    }
+    let files = &args[1..];
+    println!("processing files: {:?}", files);
 }
